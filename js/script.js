75527544dcbc0811b353a1ed3aa41be53f156093
Alex Shive <darknight@gmail.com>
2013-11-18 20:50:25 -0800

    Slider added Contact details added Portfolio done TODO: Contact form needs to be working TODO: Profile picture needs update

--- js/script.js
+++ js/script.js
@@ -2,19 +2,42 @@
 var A = {
 	init: function() {
 		this.nav.init();
+		this.email.init();
+		this.slider.init();
 	},
 	nav: {
 		init: function() {
-			$panels = $('section.panel');
-			$nav = $('nav a');
+			var $panels = $('section.panel');
+			var $nav = $('nav a');
 			$('nav a, .jump').click(function(e) {
-				$this = $(this);
+				var $this = $(this);
 				$nav.removeClass('active');
 				$panels.addClass('hidden');
 				$this.addClass('active');
 				$('#' + $this.attr('rel')).removeClass('hidden');
 				e.preventDefault();
 			});
+		}
+	},
+	email: {
+		init: function() {
+			var i = 0, 
+				output = '',
+				digits = [5,15,12,11,17,4,16,3,10,8,1,14,0,13,7,9,2,6],
+				letters = ['a','c','v','i','m','@','o','l','h','x','a','.','m','e','e','s','i','l'],
+				address = [];
+			for(i=0;i<digits.length;i+=1){
+				address[digits[i]] = letters[i]; 
+			}
+			for(i=0;i<address.length;i+=1){
+				output += address[i];
+			}
+			$('#email').attr('href', 'mailto:' + output).find('em').append(output);
+		}
+	},
+	slider: { 
+		init: function() {
+			$('.slider').unslider();
 		}
 	}
 };
